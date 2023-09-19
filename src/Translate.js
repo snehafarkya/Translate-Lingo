a413db97c11156983e8954479a7c8640d29df828
snehafarkya <snhafarkya@gmail.com>
2023-09-19 23:04:29 +0530

    Copy text added

--- src/Translate.js
+++ src/Translate.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { MdGTranslate } from "react-icons/md";
-import { RiTranslate } from "react-icons/ri";
+import { AiOutlineCopy} from "react-icons/ai";
+import { TiTickOutline} from "react-icons/ti";
+
+import { RiArrowRightCircleFill, RiCopyleftFill, RiLoopRightLine, RiTranslate } from "react-icons/ri";
 import image from './digital-translator-abstract-concept-illustration_335657-3769.avif'
 import { Form, TextArea, Button } from "semantic-ui-react";
 import axios from "axios";
@@ -15,7 +17,7 @@ export default function Translate() {
     var spin = document.getElementById('spin').style.display = "none"
     var cont = document.getElementById('cont').style.display = "block"
 
-  }, 5000)
+  }, 4000)
   // get the language of text input
   const getLanguageSource = () => {
     axios
@@ -59,13 +61,27 @@ export default function Translate() {
     getLanguageSource();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [textInput]);
+
+// copy text of resultant field
+  const copy = () => {
+    var mytext = document.getElementById('text').value;
+
+    if(mytext){ navigator.clipboard.writeText(mytext);
+    // alert("Text copied");
+    var file = document.getElementById('file').style.display = "none";
+    var right = document.getElementById('right').style.display = "block";
+    }
+    else {
+      alert("Nothing to copy!")
+    }
+  }
   return (
      <>
 <div className="h-screen loader " id="spin">
 </div>
 <div className="">
     <div className="hidden h-screen" id="cont" style={{display:"none"}}>
-      <div className="md:flex md:h-screen  md:justify-between  grid grid-cols-1 mt-3 md:mt-0  md:flex-row md:items-center md:mx-44 mx-6">
+      <div className="md:flex md:h-screen  md:justify-between  grid grid-cols-1 mt-12 md:mt-0  md:flex-row md:items-center md:mx-44 mx-6">
         <div className="md:order-2">
         <img src={image} alt="Image here" className="w-[400px] md:w-auto"/>
         </div>
@@ -118,7 +134,7 @@ export default function Translate() {
           </div>
         </Form>
       </div> */}
-      <div class="min-h-screen  py-6 flex flex-col justify-center sm:py-12  " id="formm">
+      <div class="min-h-full  py-6 flex flex-col justify-center sm:py-12  " id="formm">
 	<div class="relative py-3 sm:max-w-xl mx-auto  w-[80%] rounded-xl">
 		<div
 			class="absolute inset-0 bg-gradient-to-r from-purple-300 to-purple-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl">
@@ -134,17 +150,19 @@ export default function Translate() {
 							<input autocomplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" onChange={(e) => setTextInput(e.target.value)} />
 							<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Type text to translate</label>
 						</div>
-						<div class="relative">
-							<input autocomplete="off" id="text" name="text" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" value={resultText}/>
-							<label for="text" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm" >Your translated text</label>
-						</div>
-            <select className="select placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 " onChange={languageKey}>
+
+            <select className="select placeholder-transparent h-10 w-full border-b-2 bg-white border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 " onChange={languageKey}>
               {languagesList.map((language) => {
                 return <option value={language.code}>{language.name}</option>;
               })}
             </select>
+						<div class="relative flex">
+							<input readonly autocomplete="off" id="text" name="text" type="text" class=" mt-3 peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" value={resultText}/>
+              <label for="text" class="absolute left-0 -top-6.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm" >Your translated text</label>
+              <button onClick={copy}> <AiOutlineCopy id="file" /> <TiTickOutline id="right" className="hidden"/> </button>
+            </div>
 						<div class="relative">
-							<button class="bg-purple-700 text-white px-8 py-4 hover:bg-purple-600 flex gap-1 justify-center items-center rounded-lg" onClick={translateText}>Submit</button>
+							<button class="bg-purple-700 text-white px-8 py-4 hover:bg-purple-600 flex gap-1 justify-center items-center rounded-lg" onClick={translateText}>Translate</button>
 						</div>
 					</div>
 				</div>
