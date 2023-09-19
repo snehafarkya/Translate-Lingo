e3ea52f0938462df73892bd8c0959c212f167a09
snehafarkya <snhafarkya@gmail.com>
2023-09-19 19:57:18 +0530

    Css updated

--- src/Translate.js
+++ src/Translate.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { MdGTranslate } from "react-icons/md";
 import { RiTranslate } from "react-icons/ri";
+import image from './digital-translator-abstract-concept-illustration_335657-3769.avif'
 import { Form, TextArea, Button } from "semantic-ui-react";
 import axios from "axios";
 
@@ -63,41 +64,57 @@ export default function Translate() {
 <div className="h-screen loader " id="spin">
 </div>
 <div className="">
-    <div className="hidden visiblity-none container" id="cont" style={{display:"none"}}>
-      <div className="title">
-        <h2>
+    <div className="hidden " id="cont" style={{display:"none"}}>
+      <div className="md:flex md:h-screen md:mt-[450px] md:justify-between mt-[600px] grid grid-cols-1 h-screen md:flex-row md:items-center md:mx-44 mx-6">
+        <div className="md:order-2">
+        <img src={image} alt="Image here" className="w-[400px] md:w-auto"/>
+        </div>
+      <div className="md:order-1 md:w-1/2">
+      <div className="title text-left leading-[24px]">
+        <h1 className="md:text-6xl text-[30px]">
           {" "}
-          <RiTranslate style={{ margin: "0 10px" }} />
-          Multi-Language Text Translator
-        </h2>
+          <span>
+          <RiTranslate style={{ margin: "0 10px" }} /></span>
+          Welcome to your Friendly <span className="text-purple-700">TranslateLingo:</span> Seamlessly Connect the World.
+        </h1>
+        <p className="font-medium text-xl">Unlock a World of Possibilities with Effortless Language Translation – Connect, Communicate, and Cultivate Global Relationships Like Never Before!</p>
+        <a href="#formm">
+        <button className="bg-purple-700 text-white px-12 py-4 rounded-md hover:trasition hover:bg-purple-500 hover:duration-200">Start here</button></a>
       </div>
 
-      <div className="translator-content">
-        <Form>
-          <div className="select-lang">
-            <select className="select" onChange={languageKey}>
+      
+      </div>
+      </div>
+
+      {/* form */}
+      <div className="translator-content md:w-[30%] w-[80%] mx-auto mb-40 mt-40 md:mt-0" id="formm">
+        <Form >
+          <div className="select-lang mb-6 border-solid border-purple-700 border rounded-md">
+            <select className="select rounded-xl border border-solid border-purple-700" onChange={languageKey}>
               {languagesList.map((language) => {
                 return <option value={language.code}>{language.name}</option>;
               })}
             </select>
           </div>
-          <div className="wrapper">
+          <div className="wrapper flex gap-3 flex-col">
             <Form.Field
               control={TextArea}
               placeholder="Type text to translate"
+              className="border-purple-700 border-solid rounded-md border"
               onChange={(e) => setTextInput(e.target.value)}
             />
             <Form.Field
               control={TextArea}
               placeholder="Your result translation"
               value={resultText}
+              className="border-purple-700 border-solid rounded-md border"
             />
           </div>
-          <div className="btn">
-            <Button primary onClick={translateText}>
+          <div className="btn mt-6 ">
+            <button className="bg-purple-700 text-white px-8 py-4 hover:bg-purple-600 flex gap-1 justify-center items-center rounded-lg" onClick={translateText}>
               Translate
               <MdGTranslate style={{ margin: "0 10px" }} />
-            </Button>
+            </button>
           </div>
         </Form>
       </div>
