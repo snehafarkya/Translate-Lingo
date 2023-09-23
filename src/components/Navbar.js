cc64ba0c3b5847cdee26931beaa7a8f1aa403969
snehafarkya <snhafarkya@gmail.com>
2023-09-23 20:13:57 +0530

    Links added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../logo.png'
 export default function Navbar() {
   return (
-    <div className=' md:px-40 px-6 bg-purple-100 sticky '>
+    <div className=' md:px-40 px-6 bg-purple-100 sticky py-2'>
       <img src={logo} alt="" className='h-28' />
     </div>
   )
