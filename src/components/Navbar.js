5b8043e12467fc3954e2e0f5558813a6afbdd3a2
snehafarkya <snhafarkya@gmail.com>
2023-09-28 15:24:17 +0530

    link updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
       <div className="md:px-40 pt-6 md:pt-0 px-6 bg-purple-100 sticky flex md:flex-row  justify-between items-center">
       <img src={logo} alt="" className='h-28' />
       <div className="links md:flex md:flex-row flex-col justify-center gap-8 hidden">
-        <Link to={'/#formm'} className='text-lg font-semibold hover:text-purple-900 hover:shadow-lg text-purple-700 px-4 py-2 border-2 transition duration-200 border-transparent hover:border-purple-900 rounded-lg flex items-center gap-1 group/1'> Translator <RiHome2Line className="group-hover/1:hidden"/> <RiHome2Fill className="group-hover/1:flex hidden"/> </Link>
+        <Link to={'/'} className='text-lg font-semibold hover:text-purple-900 hover:shadow-lg text-purple-700 px-4 py-2 border-2 transition duration-200 border-transparent hover:border-purple-900 rounded-lg flex items-center gap-1 group/1'> Translator <RiHome2Line className="group-hover/1:hidden"/> <RiHome2Fill className="group-hover/1:flex hidden"/> </Link>
         <Link to={'/speech-to-text'} className='text-lg font-semibold hover:text-purple-900 hover:shadow-lg text-purple-700 px-4 py-2 border-2 transition duration-200 border-transparent hover:border-purple-900 rounded-lg flex items-center gap-1 group/2'> Speak here <RiSpeakLine className="group-hover/2:hidden" /> <RiSpeakFill className="group-hover/2:flex hidden"/> </Link>
         <Link to={'/text-to-speech'} className='text-lg font-semibold hover:text-purple-900 hover:shadow-lg text-purple-700 px-4 py-2 border-2 transition duration-200 border-transparent hover:border-purple-900 rounded-lg flex items-center gap-1 group/3'> Listen here <AiOutlineAudio className="group-hover/3:hidden"/> <AiFillAudio className="group-hover/3:flex hidden"/> </Link>
 
